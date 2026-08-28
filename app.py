669eb0ad6ed1d8125911a245c17dd5df55f10808
Alessia Biris <alessia.biris@ekos.co.uk>
2026-08-28 16:39:19 +0100

    Fix indentation in Commercial Floorspace tab

--- app.py
+++ app.py
@@ -437,7 +437,6 @@ elif not st.session_state.show_results:
             commercial_floorspace_inputs[category] = st.number_input(
                 f"{category} (m²)", min_value=0.0, value=float(commercial_floorspace_inputs[category]),
                 key=f"cf_{category}")
-s
         st.divider()
         st.subheader("Purpose Built Student Accommodation")
         st.caption("Optional. Enter the number of rooms only")
